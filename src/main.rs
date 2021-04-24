59cd9f16d3f3c5e887770f667938f382b052ad0b
Luc Bergeron <psyluche@gmail.com>
2021-04-23 21:29:15 -0400

    Changed name resolution for mod ray.

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ use vec3::*;
 mod color;
 use color::Color;
 mod ray;
+use ray::{Ray, Point3};
 
-fn ray_color(r: &ray::Ray) -> Color {
-    if hit_sphere(ray::Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
         return Color::new(1.0, 0.0, 0.0);
     }
     let unit_direction: vec3::Vec3 = unit_vector(r.dir);
@@ -14,10 +15,10 @@ fn ray_color(r: &ray::Ray) -> Color {
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
 
-fn hit_sphere(center: ray::Point3, radius: f64, r: &ray::Ray) -> bool {
-    let oc = r.orig - center;
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+    let oc = center - r.orig;
     let a = vec3::dot(r.dir, r.dir);
-    let b = 2.0 * vec3::dot(oc, r.dir);
+    let b = 2.0 * vec3::dot(oc, r.dir); //Le t dans l'explication
     let c = vec3::dot(oc, oc) - radius * radius;
     let discriminant = b*b - 4.0*a*c;
     discriminant > 0.0
@@ -32,12 +33,12 @@ fn main() {
     // Camera information
     let viewport_height = 2.0;
     let viewport_width = aspect_ratio * viewport_height;
-    let focal_lenght = 1.0;
+    let focal_length = 1.0;
 
-    let origin: ray::Point3 = ray::Point3::new(0.0, 0.0, 0.0);
+    let origin: Point3 = Point3::new(0.0, 0.0, 0.0);
     let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
     let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_lenght);
+    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     let mut ray_trace = Ppm::new(image_width, image_height);
 
@@ -47,7 +48,7 @@ fn main() {
         for i in 0..image_width {
             let u = i as f64 / (image_width as f64 - 1.0);
             let v = j as f64 / (image_height as f64 - 1.0);
-            let r = ray::Ray::new(origin, lower_left_corner + u * horizontal + v * vertical - origin);            
+            let r = ray::Ray::new(origin, lower_left_corner + u * horizontal + v * vertical - origin);
 
             let pixel_color = ray_color(&r);
             let c = color::color_pixel(pixel_color.x(), pixel_color.y(), pixel_color.z());
